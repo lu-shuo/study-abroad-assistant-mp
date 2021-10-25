f534b54f398c154f7792944312f3dd0881bcea1c
MAGICBOOKPRO\Darren Lu <734876157@qq.com>
2021-10-25 21:51:14 +0800

    测试数据

---
+++ miniprogram/test/questionnaire.js
@@ -0,0 +1,364 @@
+module.exports = {
+  "_id": "9e7190f1616ce24d008b1cb50dfc9ea8",
+  "name": "留学择校评估问卷",
+  "questions": [
+    {
+      "_id": "83cfc1ac6162b096006f848515561b57",
+      "title": "您所在高校的大学类别（前100且高水平大学，请参照上交软科排名）",
+      "options": [
+        {
+          "index": "A",
+          "content": "985",
+          "smallPoint": 3,
+          "bigPoint": 10,
+          "selected": false
+        },
+        {
+          "index": "B",
+          "content": "211",
+          "smallPoint": 2.1,
+          "bigPoint": 7,
+          "selected": false
+        },
+        {
+          "index": "C",
+          "content": "前100且高水平大学",
+          "smallPoint": 1.5,
+          "bigPoint": 5,
+          "selected": false
+        },
+        {
+          "index": "D",
+          "content": "其他普通本科院校",
+          "smallPoint": 0.6,
+          "bigPoint": 2,
+          "selected": false
+        },
+        {
+          "index": "E",
+          "content": "民办本科院校",
+          "smallPoint": 0,
+          "bigPoint": 0,
+          "selected": false
+        }
+      ],
+      "qType": 0,
+      "mainSort": "知识",
+      "subSort": "学业表现"
+    },
+    {
+      "_id": "83cfc1ac6162b096006f8486779dd461",
+      "title": "您所在专业入学至今的本科总GPA（注：还未完成本科学业的测试者，建议从学院教学秘书获取）",
+      "options": [
+        {
+          "index": "A",
+          "content": "4.0以上",
+          "smallPoint": 20,
+          "bigPoint": 90
+        },
+        {
+          "index": "B",
+          "content": "3.5-3.9",
+          "smallPoint": 18,
+          "bigPoint": 81
+        },
+        {
+          "index": "C",
+          "content": "3.0-3.4",
+          "smallPoint": 16,
+          "bigPoint": 72
+        },
+        {
+          "index": "D",
+          "content": "2.5-2.9",
+          "smallPoint": 12,
+          "bigPoint": 54
+        },
+        {
+          "index": "E",
+          "content": "2.0-2.4",
+          "smallPoint": 8,
+          "bigPoint": 36
+        },
+        {
+          "index": "F",
+          "content": "1.0-1.9",
+          "smallPoint": 4,
+          "bigPoint": 18
+        }
+      ],
+      "qType": 0,
+      "mainSort": "知识",
+      "subSort": "学业表现"
+    },
+    {
+      "_id": "83cfc1ac6162b096006f8486779dd401",
+      "title": "第三题",
+      "options": [
+        {
+          "index": "A",
+          "content": "4.0以上",
+          "smallPoint": 20,
+          "bigPoint": 90
+        },
+        {
+          "index": "B",
+          "content": "3.5-3.9",
+          "smallPoint": 18,
+          "bigPoint": 81
+        },
+        {
+          "index": "C",
+          "content": "3.0-3.4",
+          "smallPoint": 16,
+          "bigPoint": 72
+        },
+        {
+          "index": "D",
+          "content": "2.5-2.9",
+          "smallPoint": 12,
+          "bigPoint": 54
+        },
+        {
+          "index": "E",
+          "content": "2.0-2.4",
+          "smallPoint": 8,
+          "bigPoint": 36
+        },
+        {
+          "index": "F",
+          "content": "1.0-1.9",
+          "smallPoint": 4,
+          "bigPoint": 18
+        }
+      ],
+      "qType": 0,
+      "mainSort": "知识",
+      "subSort": "学业表现"
+    },{
+      "_id": "83cfc1ac6162b096006f8486779dd402",
+      "title": "第四题",
+      "options": [
+        {
+          "index": "A",
+          "content": "4.0以上",
+          "smallPoint": 20,
+          "bigPoint": 90
+        },
+        {
+          "index": "B",
+          "content": "3.5-3.9",
+          "smallPoint": 18,
+          "bigPoint": 81
+        },
+        {
+          "index": "C",
+          "content": "3.0-3.4",
+          "smallPoint": 16,
+          "bigPoint": 72
+        },
+        {
+          "index": "D",
+          "content": "2.5-2.9",
+          "smallPoint": 12,
+          "bigPoint": 54
+        },
+        {
+          "index": "E",
+          "content": "2.0-2.4",
+          "smallPoint": 8,
+          "bigPoint": 36
+        },
+        {
+          "index": "F",
+          "content": "1.0-1.9",
+          "smallPoint": 4,
+          "bigPoint": 18
+        }
+      ],
+      "qType": 0,
+      "mainSort": "知识",
+      "subSort": "学业表现"
+    },
+    {
+      "_id": "83cfc1ac6162b096006f8486779dd403",
+      "title": "第五题",
+      "options": [
+        {
+          "index": "A",
+          "content": "4.0以上",
+          "smallPoint": 20,
+          "bigPoint": 90
+        },
+        {
+          "index": "B",
+          "content": "3.5-3.9",
+          "smallPoint": 18,
+          "bigPoint": 81
+        },
+        {
+          "index": "C",
+          "content": "3.0-3.4",
+          "smallPoint": 16,
+          "bigPoint": 72
+        },
+        {
+          "index": "D",
+          "content": "2.5-2.9",
+          "smallPoint": 12,
+          "bigPoint": 54
+        },
+        {
+          "index": "E",
+          "content": "2.0-2.4",
+          "smallPoint": 8,
+          "bigPoint": 36
+        },
+        {
+          "index": "F",
+          "content": "1.0-1.9",
+          "smallPoint": 4,
+          "bigPoint": 18
+        }
+      ],
+      "qType": 0,
+      "mainSort": "知识",
+      "subSort": "学业表现"
+    },
+    {
+      "_id": "83cfc1ac6162b096006f8486779dd404",
+      "title": "第六题",
+      "options": [
+        {
+          "index": "A",
+          "content": "4.0以上",
+          "smallPoint": 20,
+          "bigPoint": 90
+        },
+        {
+          "index": "B",
+          "content": "3.5-3.9",
+          "smallPoint": 18,
+          "bigPoint": 81
+        },
+        {
+          "index": "C",
+          "content": "3.0-3.4",
+          "smallPoint": 16,
+          "bigPoint": 72
+        },
+        {
+          "index": "D",
+          "content": "2.5-2.9",
+          "smallPoint": 12,
+          "bigPoint": 54
+        },
+        {
+          "index": "E",
+          "content": "2.0-2.4",
+          "smallPoint": 8,
+          "bigPoint": 36
+        },
+        {
+          "index": "F",
+          "content": "1.0-1.9",
+          "smallPoint": 4,
+          "bigPoint": 18
+        }
+      ],
+      "qType": 0,
+      "mainSort": "知识",
+      "subSort": "学业表现"
+    },
+    {
+      "_id": "83cfc1ac6162b096006f8486779dd405",
+      "title": "第七题",
+      "options": [
+        {
+          "index": "A",
+          "content": "4.0以上",
+          "smallPoint": 20,
+          "bigPoint": 90
+        },
+        {
+          "index": "B",
+          "content": "3.5-3.9",
+          "smallPoint": 18,
+          "bigPoint": 81
+        },
+        {
+          "index": "C",
+          "content": "3.0-3.4",
+          "smallPoint": 16,
+          "bigPoint": 72
+        },
+        {
+          "index": "D",
+          "content": "2.5-2.9",
+          "smallPoint": 12,
+          "bigPoint": 54
+        },
+        {
+          "index": "E",
+          "content": "2.0-2.4",
+          "smallPoint": 8,
+          "bigPoint": 36
+        },
+        {
+          "index": "F",
+          "content": "1.0-1.9",
+          "smallPoint": 4,
+          "bigPoint": 18
+        }
+      ],
+      "qType": 0,
+      "mainSort": "知识",
+      "subSort": "学业表现"
+    },
+    {
+      "_id": "83cfc1ac6162b096006f8486779dd406",
+      "title": "第八题",
+      "options": [
+        {
+          "index": "A",
+          "content": "4.0以上",
+          "smallPoint": 20,
+          "bigPoint": 90
+        },
+        {
+          "index": "B",
+          "content": "3.5-3.9",
+          "smallPoint": 18,
+          "bigPoint": 81
+        },
+        {
+          "index": "C",
+          "content": "3.0-3.4",
+          "smallPoint": 16,
+          "bigPoint": 72
+        },
+        {
+          "index": "D",
+          "content": "2.5-2.9",
+          "smallPoint": 12,
+          "bigPoint": 54
+        },
+        {
+          "index": "E",
+          "content": "2.0-2.4",
+          "smallPoint": 8,
+          "bigPoint": 36
+        },
+        {
+          "index": "F",
+          "content": "1.0-1.9",
+          "smallPoint": 4,
+          "bigPoint": 18
+        }
+      ],
+      "qType": 0,
+      "mainSort": "知识",
+      "subSort": "学业表现"
+    }
+  ]
+}
